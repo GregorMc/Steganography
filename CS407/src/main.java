1b49ae535e287e775b73ab27232deba9115e13d0
John Lennox <jl1998@hotmail.co.uk>
2019-10-01 23:47:11 +0100

    can get the type of a given file

--- CS407/src/main.java
+++ CS407/src/main.java
@@ -1,2 +1,11 @@
+import java.io.File;
 public class main {
+    public static void main(final String[] args) {
+        stegonography stego = new stegonography();
+        File coverImage = stego.getImage("CS407/MARBLES.BMP");
+        System.out.println("#ofBytes: " + stego.getFileSize(coverImage));
+//        File fileToHide = stego.getFileToHide("");
+//        stego.hideFile(coverImage, fileToHide);
+        System.out.println("file type: " + stego.getFileTypeToHide("CS407/MARBLES.BMP"));
+    }
 }
--- CS407/src/stegonography.java
+++ CS407/src/stegonography.java
@@ -1,35 +1,57 @@
+import java.io.File;
+
 public class stegonography {
-    public void hideFile(){
-        //hide the file inside the cover image
+    public void hideFile(File coverImage, File fileToHide){
+        if (enoughSpace(coverImage, fileToHide)){
+            //start hiding the image
+
+        }else{
+            System.out.println("The coverImage does not have a big enough file size to hide the given file");
+        }
+
     }
     public void revealFile(){
         //get the file from a Stego-image
     }
-    public void getImage(){
-        //not sure about what this would return
+
+    public File getImage(String imagePath){
         //get the coverImage from file system
+        File coverImage = new File(imagePath);
+        return coverImage;
     }
-    public void getFileToHide(){
+    public File getFileToHide(String filePath){
         //get file to hide from cover system
+        File fileToHide = new File(filePath);
+        return fileToHide;
     }
-    public int getFileSize(){
+    public long getFileSize(File file){
         //how many bytes in any given file
-        return 0;
+        return file.length();
     }
-    public int maxPossibleFileSize(int coverImageSize) {
+    public long maxPossibleFileSize(long coverImageSize) {
         // how many LSB are useable?
-        return 0;
+        //54 bytes reserved for file info
+        //4 Bytes for image
+        long maximum = coverImageSize - (54 + 96);
+        return maximum;
     }
-    public boolean enoughSpace (){
+    public boolean enoughSpace (File coverImage, File fileToHide){
         // is the maximum possible file size > file to be hidden file size
+        long coverImageSize = getFileSize(coverImage);
+        long fileToHideSize = getFileSize(fileToHide);
+        if(fileToHideSize < maxPossibleFileSize(coverImageSize)){
+            return true;
+        }
         return false;
     }
     public String getHiddenFileType(){
         // get the file type of hidden file within the Stego-image
         return null;
     }
-    public String getFileTypeToHide(){
+    public String getFileTypeToHide(String filepath){
         //get the file type of the file to hide
-        return null;
+        int typeStart = filepath.indexOf(".");
+        String fileType = filepath.substring((typeStart+1), filepath.length());
+        return fileType;
     }
 }
